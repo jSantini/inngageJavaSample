387bca87e7a64bba04b02fc30b05c7af7d70001b
Jean Santini <jean.santini@monnos.com>
2021-08-14 06:35:03 -0300

    update

---
+++ app/src/main/java/com/example/inngageintegrationjavasample/InngageConstants.java
@@ -0,0 +1,7 @@
+package com.example.inngageintegrationjavasample;
+
+public interface InngageConstants {
+    String inngageAppToken = "c1eebd8663c4f6427cff285c9a343a0e"; //application token retirado da plataforma Inngage
+    String inngageEnvironment = "prod";
+    String googleMessageProvider = "FCM";
+}
---
+++ app/src/main/java/com/example/inngageintegrationjavasample/MainActivity.java
@@ -0,0 +1,68 @@
+package com.example.inngageintegrationjavasample;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+
+import br.com.inngage.sdk.InngageIntentService;
+import br.com.inngage.sdk.InngageUtils;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "INNGAGE";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        handleSubscription();
+
+    }
+
+    private void handleSubscription() {
+
+        String myInngageAppToken = InngageConstants.inngageAppToken;
+        String myInngageEnvironment = InngageConstants.inngageEnvironment;
+        String myGoogleMessageProvider = InngageConstants.googleMessageProvider;
+
+        // caso o app possua campos customizáveis, descomente estas linhas, cheque-os na plataforma, substitua-os abaixo e adicione os valores de acordo com a sua aplicação
+//        JSONObject jsonCustomField = new JSONObject();
+//
+//        try {
+//
+//            jsonCustomField.put("nome", user.name);
+//            jsonCustomField.put("email", user.email);
+//            jsonCustomField.put("telefone", "");
+//            jsonCustomField.put("dataRegistro", "");
+//            jsonCustomField.put("dataNascimento", "");
+//
+//
+//      } catch (JSONException e) {
+//
+//            e.printStackTrace();
+//      }
+//        // caso possua campos customizáveis, adicione o "jsonCustomField" como na chamada abaixo
+//        InngageIntentService.startInit(
+//                this,
+//                myInngageAppToken,
+//                "Identifier", //Seu identificador
+//                myInngageEnvironment,
+//                myGoogleMessageProvider,
+//    jsonCustomField);
+
+        // caso não possua campos customizáveis até então:
+        InngageIntentService.startInit(
+                this,
+                myInngageAppToken,
+                "userIdentifier", //Seu identificador
+                myInngageEnvironment,
+                myGoogleMessageProvider);
+
+
+        InngageUtils.handleNotification(this, getIntent(), myInngageAppToken, myInngageEnvironment);
+    }
+
+}
